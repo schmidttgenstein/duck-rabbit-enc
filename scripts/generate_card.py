e0b7ac74cfc5103472489c05cab4895475f507cc
James Schmidt <aschmi40@jhu.edu>
2024-01-04 12:45:42 -0500

    bug fix on card generate

--- scripts/generate_card.py
+++ scripts/generate_card.py
@@ -7,7 +7,7 @@ from hashlib import sha512 as sha
 from Crypto.PublicKey import RSA as rsa 
 from Crypto.Cipher import AES as aes, PKCS1_OAEP as pkcs 
 from Crypto.Random import get_random_bytes 
-    
+from utils import * 
 
 
 if __name__ == "__main__":
@@ -33,25 +33,28 @@ if __name__ == "__main__":
     try: 
         key = rsa.import_key(enc_key,passphrase = str(getpass.getpass("verify password: ")))
         cont = True 
-        print("hit enter w/o providing input to escape")
-        while cont: 
-            card_key = input("entry header: ")
-            if len(card_key) == 0:
-                break
-            card_value = input("entry value: ")        
-            if len(card_value) == 0:
-                break
-            card[card_key] = card_value 
-        file_name_plain = os.path.join(card_dir,str(uid)+"_plain.txt")
-        file_name_public = os.path.join(card_dir,str(uid)+"_address.yaml")
-
-        enc_card = encrypt_plaintext(str(card),keys.public_key(),file_name_plain)
-        assert eval(decrypt_ciphertext(enc_card,keys)) == card, "encryption/decryption not functioning properly"
-        address_card = {"uid":uid,"public_key":keys.public_key().export_key(),"entity":entity}
-        with open(file_name_public,mode = "wt", encoding="utf8") as f:
-            yaml.dump(address_card,f)
+       
     except:
         print("passwords do not match!")
+        cont = False
+
+    print("hit enter w/o providing input to escape")
+    while cont: 
+        card_key = input("entry header: ")
+        if len(card_key) == 0:
+            break
+        card_value = input("entry value: ")        
+        if len(card_value) == 0:
+            break
+        card[card_key] = card_value 
+    file_name_plain = os.path.join(card_dir,str(uid)+"_plain.txt")
+    file_name_public = os.path.join(card_dir,str(uid)+"_address.yaml")
+
+    enc_card = encrypt_plaintext(str(card),keys.public_key(),file_name_plain)
+    assert eval(decrypt_ciphertext(enc_card,keys)) == card, "encryption/decryption not functioning properly"
+    address_card = {"uid":uid,"public_key":keys.public_key().export_key(),"entity":entity}
+    with open(file_name_public,mode = "wt", encoding="utf8") as f:
+        yaml.dump(address_card,f)
 
     
 
